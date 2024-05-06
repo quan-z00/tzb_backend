56627bd0c476fcbaa60d09bbc014b072d5af860b
ForestAndForest <2900221581@qq.com>
2024-05-06 16:32:20 +0800

    init

---
+++ src/main/java/com/tzb/backend/TzbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tzb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TzbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzb/backend/TzbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TzbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
